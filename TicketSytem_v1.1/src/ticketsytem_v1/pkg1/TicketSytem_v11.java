a2d30cb112a9f0428edf6138da2e927e42879048
Robert Mares <Robert Mares>
2019-04-08 18:48:54 -0600

    Se creo un Hola mundo

---
+++ TicketSytem_v1.1/src/ticketsytem_v1/pkg1/TicketSytem_v11.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ticketsytem_v1.pkg1;
+
+/**
+ *
+ * @author rober
+ */
+public class TicketSytem_v11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola");
+    }
+    
+}
